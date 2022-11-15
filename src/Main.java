281660921b19d336ce3162259ac093575f2f511a
”NevroLag” <aleksandrkonshin03@gmail.com>
2022-11-15 17:53:12 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello");
         System.out.println("Hello 2");
+        System.out.println("Hello 3");
+        System.out.println("Hello 4");
     }
 }
